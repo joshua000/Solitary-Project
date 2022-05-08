3449b10998822896dc7f7fa3e13caf59c6a068d6
Joshua Green <joshuag19@gmail.com>
2022-05-08 09:35:23 -0600

    Added Key Tester
    Python code that shows what key is pressed using tkinter.

---
+++ KeyTestTK.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root=Tk()
+
+def key_pressed(event):
+    w=Label(root,text="Key Pressed:"+event.char)
+    w.place(x=70,y=90)
+    
+root.bind("<Key>",key_pressed)
+root.mainloop()
